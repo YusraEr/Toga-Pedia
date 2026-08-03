3ec108a602be9bb0c2a3caf61481234acc9807a5
Yusra Erlangga Putra <yusra@MacBook-Air-Yusra.local>
2026-08-03 14:50:02 +0800

    trust me bro

--- src/App.jsx
+++ src/App.jsx
@@ -24,6 +24,7 @@ function App() {
             element={
               <ProtectedRoute>
                 <AdminDashboardPage />
+                {/* enough */}
               </ProtectedRoute>
             }
           />
